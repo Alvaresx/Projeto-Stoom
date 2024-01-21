c0f170a2ae21c2751830b428a6bf014fe40dd9e5
Mariana Alvares <mariana11areal@hotmail.com>
2024-01-21 19:48:25 -0300

    criando homePageView

---
+++ src/views/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+import PizzaList from "../../components/PizzaList";
+
+const HomePageView = () => (
+  <>
+    <Header />
+    <PizzaList />
+  </>
+);
+
+export default HomePageView;
