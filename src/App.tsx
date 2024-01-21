6db0ec70501e42ca348e829aee48d011c30bc51a
Mariana Alvares <mariana11areal@hotmail.com>
2024-01-20 21:11:47 -0300

    adicionando navbar no app e ajustes de scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import PizzaList from "./components/PizzaList";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <PizzaList />
       <Footer />
